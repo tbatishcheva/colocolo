b8f4f15d7db1876e3625e5bfc2900673bba29616
Tatiana Batishcheva <t.batishcheva@octonus.com>
2019-10-27 11:03:06 +0300

    add main entities, main controller, etc

--- README.md
+++ README.md
@@ -1,1 +1,50 @@
 # colocolo
+
+![](./logo.jpg)
+
+Part #1
+
+// create the new Symfony application using Composer:
+```bash
+composer create-project symfony/skeleton colocolo
+```
+
+// Install Doctrine bundle & various libraries
+```bash
+composer require symfony/orm-pack
+composer require annotations
+composer require validator
+composer require template
+composer require security-bundle
+composer require --dev maker-bundle
+```
+
+// replace info in .env
+DATABASE_URL=mysql://admin:admin@127.0.0.1:3306/colocolo
+```
+
+// Create new ProductController
+```bash
+php bin/console make:controller
+```
+
+// Symfony's web server
+```bash
+composer require server --dev
+```
+
+// run the following command
+```bash
+php bin/console server:run
+```
+
+// Create new Category entity
+```bash
+php bin/console make:entity
+```
+
+// Create new Product entity
+```bash
+php bin/console make:entity
+```
+
---
+++ src/Entity/Product.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Table(name="product")
+ * @ORM\Entity(repositoryClass="App\Repository\ProductRepository")
+ * @ORM\HasLifecycleCallbacks
+ */
+class Product
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $description;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $dateAdded;
+
+    /**
+     * @var Category
+     *
+     * @ORM\ManyToOne(targetEntity="Category")
+     * @ORM\JoinColumn(name="category_id", referencedColumnName="id")
+     */
+    private $category;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getDateAdded(): ?\DateTimeInterface
+    {
+        return $this->dateAdded;
+    }
+
+    public function setDateAdded(\DateTimeInterface $dateAdded): self
+    {
+        $this->dateAdded = $dateAdded;
+
+        return $this;
+    }
+
+    public function getCategory(): Category
+    {
+        return $this->category;
+    }
+
+    public function setCategory(Category $category): self
+    {
+        $this->category = $category;
+
+        return $this;
+    }
+}
