7f98802938ddfed4904085c5cf1038b36b3e9d43
Tatiana Batishcheva <t.batishcheva@octonus.com>
2019-10-27 15:34:08 +0300

    Showing Products

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -4,16 +4,38 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\ORM\EntityManagerInterface;
 
 class ProductController extends AbstractController
 {
+
+    /** @var EntityManagerInterface */
+    private $entityManager;
+
+    /** @var \Doctrine\Common\Persistence\ObjectRepository */
+    private $authorRepository;
+
+    /** @var \Doctrine\Common\Persistence\ObjectRepository */
+    private $productRepository;
+
+    /**
+     * @param EntityManagerInterface $entityManager
+     */
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+        $this->productRepository = $entityManager->getRepository('App:Product');
+        $this->categoryRepository = $entityManager->getRepository('App:Category');
+    }
+
     /**
      * @Route("/", name="homepage")
+     * @Route("/entries", name="entries")
      */
-    public function indexAction()
+    public function entriesAction()
     {
-        return $this->render('product/index.html.twig', [
-            'controller_name' => 'ProductController',
+        return $this->render('product/entries.html.twig', [
+            'products' => $this->productRepository->findAll()
         ]);
     }
 }
